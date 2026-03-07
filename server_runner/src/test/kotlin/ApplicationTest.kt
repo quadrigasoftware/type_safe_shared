eb1bc8113152f8165f7b441d5e1eabead48a881a
Charles McBrian <charles@quadrigasoftware.com>
2026-03-07 14:20:01 -0800

    optimize imports

--- server_runner/src/test/kotlin/ApplicationTest.kt
+++ server_runner/src/test/kotlin/ApplicationTest.kt
@@ -2,10 +2,8 @@ package com.quadrigasoftware
 
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
--- server_runner/src/test/kotlin/LoginTest.kt
+++ server_runner/src/test/kotlin/LoginTest.kt
@@ -1,6 +1,5 @@
 package com.quadrigasoftware
 
-import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
